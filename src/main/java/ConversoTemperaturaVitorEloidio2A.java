ffba0b4514f8358409218fd55ae0c02209107d67
Vitor Eloidio do Nascimento Brandão <vitoreloidio9@gmail.com>
2025-04-29 08:41:45 -0400

    Primeiro Comiti

---
+++ src/main/java/ConversoTemperaturaVitorEloidio2A.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author 2024105070086
+ */
+public class ConversoTemperaturaVitorEloidio2A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
